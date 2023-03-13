3256770f078d44d80dfcb532747a3dd704dc3309
mberndt92 <maximilian_berndt@icloud.com>
2023-03-14 07:53:47 +0900

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Maximilian Berndt on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
